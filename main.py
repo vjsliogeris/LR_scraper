1dfd38f25af22275094990281166700106082528
vjsliogeris <renamorcen@gmail.com>
2024-06-10 01:41:21 +0300

    Added safeguards and argparse for usability

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 
 from pathlib import Path
+import argparse
 
 from lrscraper import scraper
 
-results_dir = Path('results/')
+def main(args):
+    results_dir = Path(args.output)
+    scraper.scrape(results_dir)
 
-scraper.scrape(results_dir)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog=Path(__file__).name,
+                    description='Scraper the official parliament website for speeches',
+                    epilog='Vytenis :)')
+    parser.add_argument(
+            '-o', '--output',
+            default='results',
+            help='Target directory for the speeches to be saved to.')
+    args = parser.parse_args()
+    main(args)
